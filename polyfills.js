d7de5c2046c64087932f313b255e9fb533e2158a
Naomi Plasterer <nplasterer@rewatch.com>
2022-12-12 10:26:54 -0800

    test: add a bunch of pollyfills and a patch

--- polyfills.js
+++ polyfills.js
@@ -5,8 +5,23 @@ import 'react-native-get-random-values';
 import '@ethersproject/shims';
 
 import 'text-encoding';
+import 'base-64';
+import 'react-native-fetch-api';
+import 'react-native-url-polyfill/auto';
 import 'web-streams-polyfill';
 import '@azure/core-asynciterator-polyfill';
+import { polyfill as polyfillBase64 } from 'react-native-polyfill-globals/src/base64';
+import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fetch';
+import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/encoding';
+import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
+import { polyfill as polyfillURL } from 'react-native-polyfill-globals/src/url';
+
+polyfillEncoding();
+polyfillReadableStream();
+polyfillURL();
+polyfillBase64();
+polyfillFetch();
+
 
 // Necessary for @peculiar/webcrypto.
 if (!global.Buffer) {
